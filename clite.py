73dd6a28389aff07996b09c7301fada102b0e7af
Daniel Flores Rojas <danyflorojas@gmail.com>
2024-04-15 21:24:28 -0600

    Prueba enteros

--- clite.py
+++ clite.py
@@ -1,9 +1,22 @@
 import ply.lex as lex
+tokens = ['FLOAT', 'INT']
 
-tokens = [ 'INT' ]
 
-t_ignore  = ' \t'
+# Ignorar espacios y tabs
+t_ignore = ' \t'
+
+
+# Regla para enteros simples
+def t_INT(t):
+    r'\d(_?\d)*'
+    t.value = int(t.value.replace('_', ''))
+    return t
+
+
+# Error handling rule
+def t_error(t):
+    raise lex.LexError(f"Illegal character {t.value[0]}", t.value)
 
 
 def getLexer():
-  return lex.lex()
+    return lex.lex()
